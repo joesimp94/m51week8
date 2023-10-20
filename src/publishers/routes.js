e79686340d7da55f6e837b9cbeb574508de2a57f
Joe Simpson <josephsimpson94@googlemail.com>
2023-10-20 08:55:45 +0100

    enhancement: added publishers table and files src/publishers

---
+++ src/publishers/routes.js
@@ -0,0 +1,21 @@
+// const { Router } = require("express");
+// const publisherRouter = Router();
+
+// const Publisher = require("./model");
+
+// const {
+//   addPublisher,
+//   findAllPublishersBooks,
+//   findAllPublishers,
+//   findAllPublishersAuthors,
+// } = require("./controllers");
+
+// publisherRouter.post("/", addPublisher);
+
+// publisherRouter.get("/:publisher/books", findAllPublishersBooks);
+
+// publisherRouter.get("/", findAllPublishers);
+
+// publisherRouter.get("/:publisher/authors", findAllPublishersAuthors);
+
+// module.exports = publisherRouter;
